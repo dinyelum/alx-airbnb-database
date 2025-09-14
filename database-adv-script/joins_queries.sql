3fbcd4667dde54bc75cea5ae52dc1f32d640890f
Chimdinyelum <chyjohn777@gmail.com>
2025-09-14 19:32:41 +0100

    Edit 3: Week 2: Advanced SQL

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,4 @@
-SELECT u.user_id, u.first_name, u.last_name, u.email, u.phone_number, b.booking_id, b.total_price, b.status from users as u INNER join booking as b on b.user_id = u.id
+SELECT u.user_id, u.first_name, u.last_name, u.email, u.phone_number, b.booking_id, b.total_price, b.status from users as u INNER JOIN booking as b on b.user_id = u.id
 SELECT p.property_id, p.host_id, p.name, p.description, p.location, p.price_per_night, r.rating, r.comment from property as p LEFT JOIN reviews as r on r.property_id = p.id
 -- Since MySQL does not directly support FULL OUTER JOIN
 SELECT u.*
